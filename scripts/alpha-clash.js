f67bdd3b3ec2191e4d9febfdd314b4e46095b881
devshakibuddin <112326077+devshakibuddin@users.noreply.github.com>
2024-10-03 19:25:32 +0600

    continue game

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,4 +1,26 @@
 
+function handleKeyButtonPress(event){
+    const playerPressed =event.key ;
+
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+
+    if(playerPressed === expectedAlphabet){
+        console.log('you are win');
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+    }
+    else{
+        console.log('loser');
+    }
+
+}
+
+document.addEventListener('keyup',handleKeyButtonPress)
+
+
 function continueGame(){
     const alphabet = getARandomAlphabet();
     console.log(alphabet);
