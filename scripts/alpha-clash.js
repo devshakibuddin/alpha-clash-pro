51e8cff658c35182703a347552a44e699b9661a8
devshakibuddin <112326077+devshakibuddin@users.noreply.github.com>
2024-10-04 08:27:53 +0600

    game done

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -2,6 +2,10 @@
 function handleKeyButtonPress(event){
     const playerPressed =event.key ;
 
+    if( playerPressed === 'Escape'){
+        gameOver();
+    }
+
 
     const currentAlphabetElement = document.getElementById('current-alphabet');
     const currentAlphabet = currentAlphabetElement.innerText;
@@ -9,11 +13,32 @@ function handleKeyButtonPress(event){
 
     if(playerPressed === expectedAlphabet){
         console.log('you are win');
+        const currentScore = getTextElementValueById('current-score');
+        const newScore = currentScore + 1;
+
+        setTextElementValueById('current-score',newScore)
+
+
+        // another option==
+        // const currentScoreElement = document.getElementById('current-score');
+        // const currentScoreText = currentScoreElement.innerText;
+        // const currentScore = parseInt(currentScoreText);
+
+        // const newScore = currentScore + 1;
+        // currentScoreElement.innerText = newScore;
         removeBackgroundColorById(expectedAlphabet);
         continueGame();
     }
     else{
-        console.log('loser');
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+
+        const updateLife = currentLife - 1;
+        currentLifeElement.innerText = updateLife ;
+        if(updateLife === 0){
+            gameOver();
+        }
     }
 
 }
@@ -23,7 +48,6 @@ document.addEventListener('keyup',handleKeyButtonPress)
 
 function continueGame(){
     const alphabet = getARandomAlphabet();
-    console.log(alphabet);
     const currentAlphabetElement =document.getElementById('current-alphabet');
     currentAlphabetElement.innerText = alphabet;
     setBackgroundColorById(alphabet);
@@ -34,6 +58,22 @@ function continueGame(){
 
 function play(){
     hideSectionById('home-screen');
+    hideSectionById('final-score');
     showSectionById('play-ground');
+
+    setTextElementValueById('current-life',5);
+    setTextElementValueById('current-score',0);
     continueGame();
+}
+
+function gameOver(){
+    hideSectionById('play-ground');
+    showSectionById('final-score')
+
+    const lastScore = getTextElementValueById('current-score');
+    setTextElementValueById('last-score',lastScore);
+
+    const currentAlphabet = getElementTextById('current-alphabet');
+    removeBackgroundColorById(currentAlphabet);
+
 }
